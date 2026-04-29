35af76000b2d11c38bd1b7b2755a8174615e469e
pdev2116 <pdev93905@gmail.com>
2026-04-30 01:49:48 +0530

    Initial commit

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,175 @@
+const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
+const Blacklist = require("../models/blacklist.model");
+
+
+/**
+ * @name registerUserController
+ * @description register a new user, expects username, email and password in the request body
+ * @access Public
+ */
+
+
+async function registerUserController(req, res) {
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "please provide username, email and password"
+        })
+    }
+
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "Account already exists with this email address or username"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    )
+
+    res.cookie("token", token, { httpOnly: true })
+
+    res.status(201).json({
+        message: "User registered successfully",
+        token,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+/**
+ * @name loginUserController
+ * @description login a user, expects email and password in the use body
+ * @access Public
+ */
+
+
+async function loginUserController(req, res) {
+
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    )
+
+    res.cookie("token", token, { httpOnly: true })
+
+    res.status(200).json({
+        message: "User logged in successfully!",
+        token,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+/**
+ * @name logoutUserController
+ * @description logout a user, expects token in the cookie
+ * @access Public
+ */
+
+
+async function logoutUserController(req, res) {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) {
+            return res.status(400).json({
+                message: "No token found in cookie. Please login first."
+            });
+        }
+
+        await Blacklist.create({ token });
+
+        res.clearCookie("token");
+
+        res.status(200).json({
+            message: "Logout successful, token blacklisted"
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error during logout"
+        });
+    }
+}
+
+
+/**
+ * @name getMeController
+ * @description get user details
+ * @access private
+ */
+
+
+async function getMeController(req, res){
+    
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message: "User fetched successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+
+}
+
+
+
+
+module.exports = {
+    registerUserController,
+    loginUserController,
+    logoutUserController,
+    getMeController
+    
+
+}
